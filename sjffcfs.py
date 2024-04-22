eb9a16436aa16c78260d4be45e3d366009803384
gelsiiiella <140424378+gelsiiiella@users.noreply.github.com>
2024-04-22 10:19:31 +0800

    Update sjffcfs.py

--- sjffcfs.py
+++ sjffcfs.py
@@ -1,7 +1,7 @@
 from tabulate import tabulate
 
-
-print('''               Main Menu
+def main():
+    print('''               Main Menu
                 [1] First Come First Serve 
                 [2] Shortest Job First
                 [3] Exit ''')
@@ -36,23 +36,29 @@ def first_come_first_serve():
 def shortest_job_first():
     process=int(input('Input number of processes:  '))
     burstTime=[]
-    wt= [0] 
-    tat= [0] 
-    ct= [0] 
 
     for i in range(process):
         burst=int(input("Enter burst time for process {}:".format(i+1)))
         burstTime.append(burst)
-        burstTime.sort()
 
+    burstTime.sort()
+    wt= [0] * process
+    tat= [0] * process
+    ct= [0] * process
+
+    ct[0] = burstTime[0]  
+    tat[0] = burstTime[0] 
+    wt[0] = 0   
+
+    for i in range(process):
         if i > 0:
-            ct [i] = burstTime[i+1] 
+            ct [i] = ct[i-1]+ burstTime[i] 
             tat[i] = ct[i] 
             wt[i]= ct[i]-burstTime[i]
 
-            avg_wt=sum(wt)/process
-            avg_tat=sum(tat)/process
-            avg_ct = sum(ct)/process
+        avg_wt=sum(wt)/process
+        avg_tat=sum(tat)/process
+        avg_ct = sum(ct)/process
 
     headers= ['Processes','Completion Time','Turn-Around Time','Waiting Time']
     data = [[i+1,ct[i],tat[i],wt[i]] for i in range(process)]
@@ -63,13 +69,20 @@ def shortest_job_first():
     print("Average Waiting Time:",round(avg_wt))
     print("Average Completion Time:",round(avg_ct))
 
-if menu == 1:
-    first_come_first_serve()
-elif menu ==2:
-    shortest_job_first()
-elif menu == 3:
-    exit
-else:
+
+try:
+    if menu == 1:
+        first_come_first_serve()
+    elif menu ==2:
+        shortest_job_first()
+    elif menu == 3:
+        valid = input('Are you to exit? ')
+        if valid == 'Y':
+            exit
+        else:
+            main()
+
+except:
     print('Invalid Input')
 
 
